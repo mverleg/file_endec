b3ea112bd33c1378f8e04385c73c677d28aa0568
Mark <mverleg.noreply@gmail.com>
2020-04-19 16:48:01 +0200

    Attempt to send stdin to e2e test runner

--- src/e2e.rs
+++ src/e2e.rs
@@ -7,23 +7,87 @@ use std::path::PathBuf;
 
 #[test]
 fn large_file() {
+    let key = "pass:abc123";
     let (tmp, file) = write_test_file(128 * 1024 * 1024);
-    test_encrypt(&vec![file.as_path()], &["-k", "pass:abc123qwerty987654321", "-d", "-v"]);
-    test_decrypt(&vec![file.as_path()], &["-k", "pass:abc123qwerty987654321", "-d", "-v"]);
+    let enc_pth = {
+        let mut p = file.clone();
+        p.set_file_name(format!("{}.enc", file.file_name().unwrap().to_string_lossy()));
+        p
+    };
+    test_encrypt(&vec![file.as_path()], &["-k", key, "--accept-weak-key", "-d", "-v"], None);
+    assert!(enc_pth.as_path().exists());
+    assert!(!file.as_path().exists());
+    test_decrypt(&vec![file.as_path()], &["-k", key, "-d", "-v"], None);
+    assert!(!enc_pth.as_path().exists());
+    assert!(file.as_path().exists());
     tmp.close().unwrap()
 }
 
 #[test]
 fn many_files() {
+    let key = "!&R$ Eq1\n473L19XTGK'K7#be7\0Rl b62U8R2";
     let files: Vec<(TempDir, PathBuf)> = (50..200)
         .map(|i| write_test_file(i * 1024))
         .collect();
     let paths: Vec<_> = files.iter().map(|f| f.1.as_path()).collect();
-    test_encrypt(&paths, &["-k", "pass:abc123qwerty987654321", "-v"]);
-    test_decrypt(&paths, &["-k", "pass:abc123qwerty987654321", "-v"]);
+    test_encrypt(&paths, &["-k", "pipe", "-q"], Some(key.to_owned()));
+    paths.iter().for_each(|p| assert!(p.exists()));
+    test_decrypt(&paths, &["-k", "pipe", "-q"], Some(key.to_owned()));
     files.into_iter().for_each(|f| f.0.close().unwrap());
 }
 
 //TODO @mark: try all/most CLI args
 //TODO @mark: can I try version compatibiltiy here?
+
+
+//pass:$password
+//env:$var_name
+//file:$path
+//ask
+//askonce
+//pipe
+
+
+// USAGE:
+// fileenc [FLAGS] [OPTIONS] <FILES>...
+//
+// FLAGS:
+// --accept-weak-key    Suppress warning if the encryption key is not strong.
+// --dry-run            Test encryption, but do not save encrypted files (nor delete input, if --delete-input).
+// -h, --help               Prints help information
+// -f, --overwrite          Overwrite output files if they exist.
+//
+// OPTIONS:
+// -k, --key <key-source>
+// Where to get the key; one of 'pass:$password', 'env:$var_name', 'file:$path', 'ask', 'askonce', 'pipe'
+// [default: ask]
+// -o, --output-dir <output-dir>
+// Alternative output directory. If not given, output is saved alongside input.
+//
+// --output-extension <output-extension>    Extension added to encrypted files. [default: .enc]
+//
+// ARGS:
+// <FILES>...    One or more paths to input files (absolute or relative)
+//
+// @simba # :file_endec$ cargo run --release --bin filedec -- -h
+// Finished release [optimized] target(s) in 0.13s
+// Running `target/release/filedec -h`
+// FileEnc 1.0.0
+// github.com/mverleg/file_endec
+// Securely encrypt one or more files using the given key.
+//
+// USAGE:
+// filedec [FLAGS] [OPTIONS] <FILES>...
+//
+// FLAGS:
+// -h, --help            Prints help information
+// -f, --overwrite       Overwrite output files if they exist.
+//
+// OPTIONS:
+// -k, --key <key-source>           Where to get the key; one of 'pass:$password', 'env:$var_name', 'file:$path',
+// 'ask', 'askonce', 'pipe' [default: ask]
+// -o, --output-dir <output-dir>    Alternative output directory. If not given, output is saved alongside input.
+//
+// ARGS:
+// <FILES>...    One or more paths to encrypted input files (absolute or relative)
 
--- src/util/test_cmd.rs
+++ src/util/test_cmd.rs
@@ -4,35 +4,46 @@ use ::std::ffi::OsStr;
 use ::std::path::Path;
 use ::std::process::Command;
 use ::std::str::from_utf8;
+use ::std::process::Stdio;
+use ::std::io::{Read, Write};
 
-pub fn test_cmd<I, S>(args: I) -> String
+pub fn test_cmd<I, S>(args: I, input: Option<String>) -> String
     where
         I: IntoIterator<Item = S>,
         S: AsRef<OsStr> {
-    let enc_out = Command::new("cargo")
+    let mut command = Command::new("cargo")
         .args(args)
-        .output()
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
         .unwrap();
-    let err = from_utf8(&enc_out.stderr).unwrap();
+    if let Some(txt) = input {
+        command.stdin.unwrap().write(txt.as_bytes());
+    }
+    let mut buffer = Vec::new();
+    command.stderr.unwrap().read_to_end(&mut buffer).unwrap();
+    let err = from_utf8(&buffer).unwrap();
     if !err.is_empty() {
         eprintln!("{}", err);
     }
-    assert!(enc_out.status.success());
-    let out = from_utf8(&enc_out.stdout).unwrap().to_owned();
-    println!("{}", out);
+    command.stdout.unwrap().read_to_end(&mut buffer).unwrap();
+    let out = from_utf8(&buffer).unwrap().to_owned();
+    println!("{}", &out);
+    assert!(command.wait().unwrap().success());
     out
 }
 
-pub fn test_encrypt(paths: &[&Path], nonfile_args: &[&str]) -> String {
+pub fn test_encrypt(paths: &[&Path], nonfile_args: &[&str], input: Option<String>) -> String {
     let mut args = vec!["run", "--release", "--bin", "fileenc", "--"];
     for pth in paths {
         args.push(pth.to_str().unwrap());
     }
     args.extend_from_slice(nonfile_args);
-    test_cmd(args)
+    test_cmd(args, input)
 }
 
-pub fn test_decrypt(paths: &[&Path], nonfile_args: &[&str]) -> String {
+pub fn test_decrypt(paths: &[&Path], nonfile_args: &[&str], input: Option<String>) -> String {
     let mut args = vec![
         "run".to_owned(),
         "--release".to_owned(),
@@ -45,5 +56,5 @@ pub fn test_decrypt(paths: &[&Path], nonfile_args: &[&str]) -> String {
         .for_each(|p| args.push(p));
     nonfile_args.into_iter()
         .for_each(|a| args.push((*a).to_owned()));
-    test_cmd(args)
+    test_cmd(args, input)
 }

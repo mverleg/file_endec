d5f43a50153060d62e82e6729482a77b86f0d4d2
Mark <mverleg.noreply@gmail.com>
2020-12-05 21:25:11 +0100

    logging to better tune timing

--- src/progress/log.rs
+++ src/progress/log.rs
@@ -6,6 +6,7 @@ use crate::progress::Progress;
 
 pub struct LogProgress {
     current: String,
+    current_weight: usize,  //TODO @mark: TEMPORARY! REMOVE THIS!
     started_at: Instant,
 }
 
@@ -13,13 +14,15 @@ impl LogProgress {
     pub fn new() -> Self {
         LogProgress {
             current: "initializing".to_owned(),
+            current_weight: 1,
             started_at: Instant::now(),
         }
     }
 
-    fn next(&mut self, next: String) {
+    fn next_step(&mut self, next: String) {
         let duration = self.started_at.elapsed().as_millis();
         println!("> finish {} ({} ms)", &self.current, duration);
+        println!("== {}: {} ms for {} units, so {:.3} ms/u", &self.current, duration, self.current_weight, (self.started_at.elapsed().as_micros() as f64 * 1e-3) / (self.current_weight as f64));
         println!("< start  {}", &next);
         self.current = next;
         self.started_at = Instant::now();
@@ -29,26 +32,26 @@ impl LogProgress {
 impl Progress for LogProgress {
     fn start_stretch_alg(&mut self, alg: &KeyHashAlg, file: Option<&FileInfo>) {
         if let Some(file) = file {
-            self.next(format!(
+            self.next_step(format!(
                 "stretching key for {} using {}",
                 file.file_name(),
                 alg
             ));
         } else {
-            self.next(format!("stretching key using {}", alg));
+            self.next_step(format!("stretching key using {}", alg));
         }
     }
 
     fn start_read_for_file(&mut self, file: &FileInfo) {
-        self.next(format!("reading {}", file.file_name()));
+        self.next_step(format!("reading {}", file.file_name()));
     }
 
     fn start_private_header_for_file(&mut self, file: &FileInfo) {
-        self.next(format!("private header {}", file.file_name()));
+        self.next_step(format!("private header {}", file.file_name()));
     }
 
     fn start_compress_alg_for_file(&mut self, alg: &CompressionAlg, file: &FileInfo) {
-        self.next(format!(
+        self.next_step(format!(
             "(de)compressing {} using {}",
             file.file_name(),
             alg
@@ -56,25 +59,25 @@ impl Progress for LogProgress {
     }
 
     fn start_sym_alg_for_file(&mut self, alg: &SymmetricEncryptionAlg, file: &FileInfo) {
-        self.next(format!("en/decrypting {} using {}", file.file_name(), alg));
+        self.next_step(format!("en/decrypting {} using {}", file.file_name(), alg));
     }
 
     fn start_checksum_for_file(&mut self, file: &FileInfo) {
-        self.next(format!("calculating checksum {}", file.file_name()));
+        self.next_step(format!("calculating checksum {}", file.file_name()));
     }
 
     fn start_write_for_file(&mut self, file: &FileInfo) {
-        self.next(format!(
+        self.next_step(format!(
             "writing {}",
             file.out_pth.file_name().unwrap().to_string_lossy()
         ));
     }
 
     fn start_shred_input_for_file(&mut self, file: &FileInfo) {
-        self.next(format!("shred input {}", file.file_name()));
+        self.next_step(format!("shred input {}", file.file_name()));
     }
 
     fn finish(&mut self) {
-        self.next("finishing up".to_owned());
+        self.next_step("finishing up".to_owned());
     }
 }
